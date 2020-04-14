de14251ee7a577b1ddd947b4a946499b22d1b305
Yogesh Narang <yogesh.narang49@gmail.com>
2020-04-14 22:40:02 +0530

    Name Functionality Added

--- Sheets-Editor/myscripts.js
+++ Sheets-Editor/myscripts.js
@@ -1,3 +1,14 @@
+//defining doc name
+
+var docName = "Untitled Spreadsheet (Click to Edit) ";
+document.getElementById("doc_name").innerHTML = docName;
+document.querySelector('#doc_name').addEventListener('click', function () {
+   docName = prompt("Please enter name of Document ", "<name goes here>");
+    if (docName!= null) {
+    document.getElementById("doc_name").innerHTML = docName;
+    }
+});
+
 // Defining the function to create empty data
 
 function creatEmptyDataArray(x, y) {
@@ -130,3 +141,4 @@ document.querySelector('.load').addEventListener('click', function () {
     data=dataSaved;
     hot.render();
 });
+
